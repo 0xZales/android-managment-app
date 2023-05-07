3192fe1cda423cfa40408f964304f103efd04a9f
0xZales <rozalesamiral@gmail.com>
2023-05-07 20:17:16 +0000

    Registration and DatabaseClass implemented

--- app/src/main/java/com/example/tiggestion/DatabaseClass.java
+++ app/src/main/java/com/example/tiggestion/DatabaseClass.java
@@ -1,6 +1,8 @@
 package com.example.tiggestion;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -15,11 +17,42 @@ public class DatabaseClass extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create Table users(email TEXT primary key,password TEXT)");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists users");
+    }
+
+    public Boolean createUser(String email,String password){
+        SQLiteDatabase database = this.getReadableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("email",email);
+        contentValues.put("password",password);
+        long result = database.insert("users",null,contentValues);
+        if(result==-1) return false;
+        return true;
+
+
+    }
+    public Boolean checkEmail(String email){
+        SQLiteDatabase database = this.getReadableDatabase();
+        Cursor cursor = database.rawQuery("Select * from users where email = ?",new String[]{email});
+
+        if(cursor.getCount()>0) return true;
+        return false;
+
+
+    }
+    public Boolean authentication(String email,String password){
+        SQLiteDatabase database = this.getReadableDatabase();
+        Cursor cursor = database.rawQuery("Select * from users where email = ? and password=?",new String[]{email,password});
+
+        if(cursor.getCount()>0) return true;
+        return false;
+
 
     }
 }
--- app/src/main/java/com/example/tiggestion/Registration.java
+++ app/src/main/java/com/example/tiggestion/Registration.java
@@ -2,14 +2,17 @@ package com.example.tiggestion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registration extends AppCompatActivity {
 EditText email,password,repassword;
 Button signup,exit;
+DatabaseClass DB ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +21,37 @@ Button signup,exit;
         this.password=(EditText) findViewById(R.id.password);
         this.repassword=(EditText) findViewById(R.id.repassword);
         this.signup =(Button) this.findViewById(R.id.signup);
+        this.exit =(Button) this.findViewById(R.id.exit);
+        DB = new DatabaseClass(this);
+
+
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String inputEmail= email.getText().toString();
+                String inputPassword= password.getText().toString();
+                String inputRepassword= repassword.getText().toString();
+
+                if(inputEmail.equals("")|| inputRepassword.equals("")||inputPassword.equals("")){
+                    Toast.makeText(Registration.this,"Veuilez remplir les champs",Toast.LENGTH_SHORT).show();
+                } else {
+                    if(inputPassword.equals(inputRepassword)){
+                        Boolean checkEmail = DB.checkEmail(inputEmail);
+                        if(!checkEmail){
+                            Boolean register = DB.createUser(inputEmail,inputPassword);
+                            if(register){
+                                Toast.makeText(Registration.this,"Compte crée avec sucess",Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(Registration.this,"Erreur survenu lors de l'inscription ",Toast.LENGTH_SHORT).show();
+                            }
+                        } else{
+                            Toast.makeText(Registration.this,"Vous avez deja un compte",Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(Registration.this,"Mot de passe differents",Toast.LENGTH_SHORT).show();
+                    }
+                }
+
 
             }
         });
